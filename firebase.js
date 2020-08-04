aeeb5a3bb24442c9e56256c3e16c9f45d3412b34
Saurabh Mishra <saurabhmimshra@gmail.com>
2020-08-04 23:33:40 +0530

    ADD: Initial commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDpgr4tFfvb03rlDP8C4JqNRDvjohto_jE",
+  authDomain: "tik-tok-clone-2ea8f.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-2ea8f.firebaseio.com",
+  projectId: "tik-tok-clone-2ea8f",
+  storageBucket: "tik-tok-clone-2ea8f.appspot.com",
+  messagingSenderId: "470478150332",
+  appId: "1:470478150332:web:c5218d0d14764fb9d50eb9",
+  measurementId: "G-3FFDMW9GB2",
+};
