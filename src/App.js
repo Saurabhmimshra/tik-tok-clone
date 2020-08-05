4e03bfb3eb81069382564e13d94936ceaae12c71
Saurabh Mishra <saurabhmimshra@gmail.com>
2020-08-05 16:56:02 +0530

    ADD: Complete clone

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Video from "./Video";
+import db from "./firebase";
 
 function App() {
-  return <div className="App"></div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection("videos").onSnapshot((snapshot) => {
+      setVideos(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="app__videos">
+        {videos.map(
+          ({ url, channel, song, description, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              shares={shares}
+              messages={messages}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDpgr4tFfvb03rlDP8C4JqNRDvjohto_jE",
   authDomain: "tik-tok-clone-2ea8f.firebaseapp.com",
@@ -8,3 +10,9 @@ const firebaseConfig = {
   appId: "1:470478150332:web:c5218d0d14764fb9d50eb9",
   measurementId: "G-3FFDMW9GB2",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
